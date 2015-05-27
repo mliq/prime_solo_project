a765e1fb8759b0d6ed3f4817c0a5f0cb9d5f1849
mliq <liquori@gmail.com>
2015-05-27 16:00:17 -0500

    jQuery to make incrementing z-indexes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,7 +6,15 @@ var testData = {
     "Chile": 50
 };
 
-app.controller("IndexController", ['$scope', '$http', function($scope, $http){
+app.controller("IndexController", ['$scope', '$http', function ($scope, $http) {
     $scope.data = testData;
     console.log($scope.data);
 }]);
+
+$(document).ready(function () {
+// Increment z-indexes
+    $('.div').each(function (index) {
+        $(this).css("z-index", index);
+    });
+    $('.container').css('height','+=20em');
+});
