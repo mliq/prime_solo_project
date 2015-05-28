8ee0e7d66654d252e50a6bbd51c0ef77402dc6a4
mliq <liquori@gmail.com>
2015-05-28 13:47:49 -0500

    File upload working with multer

--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var xlsxj = require("xlsx-to-json");
 var path = require('path');
-//var fs = require('fs');
+var multer = require('multer');
 var router = express.Router();
 
 router.get('/upload', function(req,res,next) {
+    res.render('upload', { title: 'XLSX Upload' });
+
     xlsxj({
         input: path.resolve(__dirname, "../public/data/data.xlsx"),
         output: path.resolve(__dirname, "../public/data/data.json")
@@ -13,10 +15,19 @@ router.get('/upload', function(req,res,next) {
             console.log(err);
         }else {
             console.log(result);
+
             res.sendFile(path.resolve(__dirname, '../public/data/data.json'));
         }
     });
 });
+
+router.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+    console.dir(req.files);
+    console.log(req.body) // form fields
+    console.log(req.files) // form files
+    res.status(204).end()
+}]);
+
 
 router.get('/json', function(req,res,next) {
     res.sendFile(path.resolve(__dirname, '../public/data/data.json'));
