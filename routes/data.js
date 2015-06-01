bcbf3c4a743455855c205da29396e80bb0a8290c
mliq <liquori@gmail.com>
2015-06-01 09:34:05 -0500

    upload modal and flash message works

--- routes/data.js
+++ routes/data.js
@@ -4,45 +4,49 @@ var path = require('path');
 var multer = require('multer');
 var router = express.Router();
 
-router.get('/upload', function(req,res,next) {
-    if (req.isAuthenticated() == true) {
-        if(req.user.group==="admin"){
-            res.render('upload', { title: 'XLSX Upload',messages: req.flash('fileUpload') });
-        } else {
-            res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
-        }
-    } else {
-        res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
-    }
-});
+//router.get('/upload', function(req,res,next) {
+//    if (req.isAuthenticated() == true) {
+//        if(req.user.group==="admin"){
+//            res.render('upload', { title: 'XLSX Upload',messages: req.flash('fileUpload') });
+//        } else {
+//            res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
+//        }
+//    } else {
+//        res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
+//    }
+//});
 
-router.post('/upload',[ multer({
-    dest: './uploads/',
-    rename: function(){return 'data'}}),
-    function(req, res){
-    console.dir(req.files);
-    console.log(req.body); // form fields
-    console.log(req.files); // form files
-    xlsxj({
-        input: path.resolve(__dirname, "../uploads/data.xlsx"),
-        output: path.resolve(__dirname, "../public/data/data.json")
-    }, function(err, result) {
-        if(err) {
-            console.log(err);
-        }else {
-            console.log(result);
+router.post('/upload', [
+    multer({
+        dest: './uploads/',
+        rename: function () {
+            return 'data'
         }
-    });
-    req.flash('fileUpload', 'Success');
-    res.redirect('/data/upload');
-    res.status(204).end()
-}]);
+    }),
+    function (req, res) {
+        console.dir(req.files);
+        console.log(req.body); // form fields
+        console.log(req.files); // form files
+        xlsxj({
+            input: path.resolve(__dirname, "../uploads/data.xlsx"),
+            output: path.resolve(__dirname, "../public/data/data.json")
+        }, function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+            }
+        });
+        req.flash('fileUpload', 'Upload Successful!');
+        res.redirect('/users');
+        res.status(204).end()
+    }]);
 
-router.get('/json', function(req,res,next) {
+router.get('/json', function (req, res, next) {
     if (req.isAuthenticated() == true) {
-            res.sendFile(path.resolve(__dirname, '../public/data/data.json'));
+        res.sendFile(path.resolve(__dirname, '../public/data/data.json'));
     } else {
-        res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
+        res.render('login', {title: '3M IPD 2015 Sales Contest', messages: req.flash('loginMessage')});
     }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,27 @@ var router = express.Router();
 var Users = require('../models/user');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     if (req.isAuthenticated() == true) {
-        if(req.user.group==="admin"){
-            res.render('admin', {title: '3M IPD 2015 Sales Contest'});
+        if (req.user.group === "admin") {
+            res.render('admin', {title: '3M IPD 2015 Sales Contest', messages: req.flash('fileUpload')});
         } else {
             res.render('index', {title: '3M IPD 2015 Sales Contest'});
         }
     } else {
-        res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
+        res.render('login', {title: '3M IPD 2015 Sales Contest', messages: req.flash('loginMessage')});
     }
 });
 
 /* GET users list. */
 router.get('/list', function (req, res, next) {
-    if (req.isAuthenticated() == true && req.user.group==='admin') {
+    if (req.isAuthenticated() == true && req.user.group === 'admin') {
         Users.find(function (err, users) {
             if (err) return next(err);
             res.json(users);
         });
     } else {
-        res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
+        res.render('login', {title: '3M IPD 2015 Sales Contest', messages: req.flash('loginMessage')});
     }
 });
 
