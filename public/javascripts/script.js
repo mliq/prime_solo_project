64465e7122edbf7eaaed53bb995713cf39311fe5
mliq <liquori@gmail.com>
2015-06-03 18:02:37 -0500

    Fixed double loading issue

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,12 +1,26 @@
 var app = angular.module('app', ['ui.grid']);
 
+function sortByPc(a,b) {
+    if (a.percent < b.percent)
+        return -1;
+    if (a.percent > b.percent)
+        return 1;
+    return 0;
+}
+
 app.controller("IndexController", ['$scope', '$http', function ($scope, $http) {
 
     $http.get('/data/json').
         success(function (data) {
             console.log(data);
-            $scope.myData = data;
+            $scope.myData = data.sort(sortByPc);
+            console.log($scope.myData);
         });
+
+    //Remove first 5 rows from table data.
+    // First sort. If sorting here, don't sort in html.
+
+    //$scope.gridOpts.data.splice(0,1);
 
     $scope.myOptions = {
         data: 'myData',
