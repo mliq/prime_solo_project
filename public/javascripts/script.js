587977a191dfda3a8e728bded60146238e9f595a
mliq <liquori@gmail.com>
2015-06-04 13:36:45 -0500

    Finally got small svg to show in table

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,26 +8,30 @@ function sortByPc(a,b) {
     return 0;
 }
 
-var svgSmall = "<img src='/svg'>";
+var svgSmall = '<svg width="25" height="25"> <circle id="svg_1" r="4" cy="6.25" cx="6.25" stroke-width="1.5" stroke="#000000" fill="none"></circle> <circle id="svg_2" r="4" cy="6.25" cx="18.75" stroke-width="1.5" stroke="#000000" fill="none"></circle> <circle id="svg_3" r="4" cy="18.75" cx="6.25" stroke-width="1.5" stroke="#000000" fill="none"></circle> <circle id="svg_4" r="4" cy="18.75" cx="18.75" stroke-width="1.5" stroke="#000000" fill="none"></circle></svg>';
+var goalsTemplate = "<img ng-src='/svg'>";
+    //'<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(\'age\')}}">Visible text</a></div>';
 
 app.controller("IndexController", ['$scope', '$http', function ($scope, $http) {
 
     $http.get('/data/json').
         success(function (data) {
-            console.log(data);
             $scope.myData = data.sort(sortByPc);
 
             //Remove first 5 rows from table data.
             $scope.tableData = $scope.myData.splice(5);
+
             // Add Images
-            $scope.tableData[9].image = svgSmall;
-            $scope.columns = [
-                { name: 'Image', displayName: 'Goals'},
-                { name: 'region', displayName: 'Country' },
-                { name: 'percent', displayName: 'Progress', visible: true }
-                ]
-            $scope.columns.push({ field: 'Image'});
+            console.log($scope.tableData);
+            //$scope.tableData[9].image = svgSmall;
         });
+
+    $scope.columns = [
+        { field: 'Image', name: 'Goals', visible: false},
+        { name: 'Goals', cellTemplate: svgSmall},
+        { field: 'region', name: 'Country' },
+        { field: 'percent', name: 'Progress'}
+    ];
 
     $scope.myOptions = {
         data: 'tableData',
