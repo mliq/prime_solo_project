079aa27c6786075fc0bafecabb7d52cb09434648
mliq <liquori@gmail.com>
2015-06-04 10:29:50 -0500

    SVGs of four circles appear basically how i want

--- routes/data.js
+++ routes/data.js
@@ -4,18 +4,6 @@ var path = require('path');
 var multer = require('multer');
 var router = express.Router();
 
-//router.get('/upload', function(req,res,next) {
-//    if (req.isAuthenticated() == true) {
-//        if(req.user.group==="admin"){
-//            res.render('upload', { title: 'XLSX Upload',messages: req.flash('fileUpload') });
-//        } else {
-//            res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
-//        }
-//    } else {
-//        res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
-//    }
-//});
-
 router.post('/upload', [
     multer({
         dest: './uploads/',
@@ -24,9 +12,9 @@ router.post('/upload', [
         }
     }),
     function (req, res) {
-        console.dir(req.files);
-        console.log(req.body); // form fields
-        console.log(req.files); // form files
+        //console.dir(req.files);
+        //console.log(req.body); // form fields
+        //console.log(req.files); // form files
         xlsxj({
             input: path.resolve(__dirname, "../uploads/data.xlsx"),
             output: path.resolve(__dirname, "../public/data/data.json")
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res, next) {
     res.render('login', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
 });
 
+router.get('/svg', function(req, res, next) {
+    res.render('svg', { title: '3M IPD 2015 Sales Contest',messages: req.flash('loginMessage') });
+});
+
 // passport.authenticate is specifying our ‘local’ strategy that we created, and specifies a failure and success redirect.
 router.post('/', passport.authenticate('local', {
         successRedirect: '/users',
